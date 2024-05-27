1dd8fe5e8eb74ba98cf4cb35aa09c94cb0e02062
pedroguyot <pguyot@udesa.edu.ar>
2024-05-27 19:02:53 -0300

    Termine la actividad de eventos

--- js/main.js
+++ js/main.js
@@ -32,10 +32,91 @@ window.addEventListener('load', function() {
  
     /* Capturar pesonajes + Asignarles a cada uno su evento correspondiente, 
     así como también su funcionalidad */
-    
+
+    let bart = this.document.querySelector('#bart');
+    bart.addEventListener('click', function() {
+        alert('Seleccionaste a Bart!')
+        bart.classList.toggle('personaje-seleccionado');
+    })
+
+    let homero = this.document.querySelector('#homero');
+    homero.addEventListener('click', function() {
+        alert('Seleccionaste a Homero!')
+        homero.classList.toggle('personaje-seleccionado')
+    })
+
+    let milhouse = this.document.querySelector('#milhouse');
+    milhouse.addEventListener('click', function() {
+        alert('Seleccionaste a Milhouse!')
+        milhouse.classList.toggle('personaje-seleccionado')
+    })
+
+    let nelson = this.document.querySelector('#nelson');
+    nelson.addEventListener('click', function() {
+        alert('Seleccionaste a Nelson!')
+        nelson.classList.toggle('personaje-seleccionado')
+    })
+
+    let lisa = this.document.querySelector('#lisa');
+    lisa.addEventListener('dblclick', function() {
+        alert('Seleccionaste a Lisa!')
+        lisa.classList.toggle('personaje-seleccionado')
+    })
+
+    let duffman = this.document.querySelector('#duffman');
+    duffman.addEventListener('dblclick', function() {
+        alert('Seleccionaste a Duffman!')
+        duffman.classList.toggle('personaje-seleccionado')
+    })
+
+    let bobPatinio = this.document.querySelector('#bobPatinio');
+    bobPatinio.addEventListener('dblclick', function() {
+        alert('Seleccionaste a Bob Patinio!')
+        bobPatinio.classList.toggle('personaje-seleccionado')
+    })
+
+    let marge = this.document.querySelector('#marge');
+    marge.addEventListener('mouseenter', function() {
+        alert('Entraste en el area de Marge!')
+        marge.classList.toggle('personaje-seleccionado')
+    })
+
+    let burns = this.document.querySelector('#burns');
+    burns.addEventListener('mouseenter', function() {
+        alert('Entraste en el area de Burns!')
+        burns.classList.toggle('personaje-seleccionado')
+    })
+
+    let flanders = this.document.querySelector('#flanders');
+    flanders.addEventListener('mouseenter', function() {
+        alert('Entraste en el area de Flanders!')
+        flanders.classList.toggle('personaje-seleccionado')
+    })
+
+    let gorgory = this.document.querySelector('#gorgory');
+    gorgory.addEventListener('mouseenter', function() {
+        alert('Entraste en el area de Gorgory!')
+        gorgory.classList.toggle('personaje-seleccionado')
+    })
+
+    let maggie = this.document.querySelector('#maggie');
+    maggie.addEventListener('mouseleave', function() {
+        alert('Saliste del area de Maggie!')
+        maggie.classList.toggle('personaje-seleccionado')
+    })
+
 
     /* Asignarle evento a la variable boton */
-    let boton = 'a';
+    let boton = document.querySelector('.boton');
+
+    boton.addEventListener('click', function() {
+        let seleccionados = document.querySelectorAll('personaje-seleccionado');
+        console.log('Personajes seleccionados:');
+        seleccionados.forEach(function(personaje) {
+            console.log(personaje.querySelector('p').textContent);
+    
+        })
+    })
  
  
  }) 
